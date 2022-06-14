77582a1cba3aad50ad53715407b3a686d0e77242
Gustavo Nascimento Souza <gustavo.ns@outlook.com.br>
2022-06-13 23:01:13 -0300

    :pencil: Aula 5 - Finishing project! Added Media-Queries

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,14 +36,21 @@ const swiper = new Swiper('.swiper', {
     },
 
     mousewheel: true,
-    keyboard: true
+    keyboard: true,
+
+    breakpoints: {
+        767: {
+            slidesPerView: 2,
+            setWrapperSize: true
+        }
+    }
 });
 
 /*===== SCROLL REVEAL =====*/
 const scrollReveal = ScrollReveal({
     origin: 'top',
     distance: '30px',
-    duration: 700,
+    duration: 500,
     reset: true
 })
 
